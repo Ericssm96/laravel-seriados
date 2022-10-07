73b21a1c206581b56f8928aaf35392c151fd8936
Eric Stefano Silva Milhomem <94946025+Ericssm96@users.noreply.github.com>
2022-10-07 11:55:26 -0300

    Otimizei a validação exibindo mensagens de erro

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -10,6 +10,20 @@
 </head>
 <body>
 <h1 class="bg-dark text-center text-white">{{ $title }}</h1>
-<div class="container"><div class="col-12 align-self-center">{{$slot}}</div></div>
+
+<div class="container">
+    <div class="col-12 align-self-center">
+        @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+        {{$slot}}
+    </div>
+</div>
 </body>
 </html>
--- resources/views/components/series/form.blade.php
+++ resources/views/components/series/form.blade.php
@@ -1,7 +1,7 @@
 <form action="{{ $action }}" method="post">
     @csrf
 
-    @isset($nome)
+    @isset($update)
         @method('put')
     @endisset
 
